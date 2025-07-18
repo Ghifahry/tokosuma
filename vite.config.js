d76840351c5cb81d5f1065862434690c58b1dd6c
Ghifahry <eghifahry@gmail.com>
2025-07-18 15:13:22 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // ⬅️ ini penting!
+      base: "/navbar-footer-submenuaccount-vue.jsn/",
     },
   },
 });
